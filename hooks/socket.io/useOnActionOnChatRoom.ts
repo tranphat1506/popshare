39bbf9db3dd70cf7f2f1e7681a094b8203d84e65
Tran Phat <lechautranphat@gmail.com>
2024-09-23 08:17:52 +0700

    FIX: fixing when user is out from chat box

--- hooks/socket.io/useOnActionOnChatRoom.ts
+++ hooks/socket.io/useOnActionOnChatRoom.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { socketConnection, SocketEvent } from '@/lib/SocketFactory';
 import { IMessageDetail, IMessageTypeTypes } from '@/redux/chatRoom/messages.interface';
-import { useAppSelector } from '@/redux/hooks/hooks';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks/hooks';
+import { updateChatRoomData } from '@/redux/chatRoom/reducer';
 
 export interface IResponseOnTyping extends IRequestOnTyping {
     userId: string;
@@ -14,14 +15,25 @@ export interface IRequestOnTyping {
 const useOnActionOnChatRoom = (roomId: string = 'global') => {
     const socket = socketConnection.socket;
     const [action, setAction] = useState<IResponseOnTyping>();
+    const dispatch = useAppDispatch();
     const user = useAppSelector((state) => state.auth.user);
     useEffect(() => {
         socket.on(SocketEvent.responseTyping, (action: IResponseOnTyping) => {
-            console.log(action);
+            // console.log(action);
             if ((roomId === action.roomId || roomId === 'global') && user && action.userId !== user.userId)
                 setAction(action);
         });
     }, []);
+    useEffect(() => {
+        if (action)
+            dispatch(
+                updateChatRoomData({
+                    roomId: action.roomId,
+                    field: 'onAction',
+                    data: action,
+                }),
+            );
+    }, [action]);
     return [action, setAction] as [
         IResponseOnTyping | undefined,
         React.Dispatch<React.SetStateAction<IResponseOnTyping | undefined>>,
@@ -32,12 +44,20 @@ const useEmitOnTyping = (roomId?: string) => {
     const socket = socketConnection.socket;
     const [type, setType] = useState<IResponseOnTyping['typeTyping'] | undefined>();
     useEffect(() => {
+        // console.log(type);
         if (type) {
             socket.emit(SocketEvent.onTyping, {
                 roomId: roomId,
                 typeTyping: type,
             } as IRequestOnTyping);
         }
+
+        return () => {
+            socket.emit(SocketEvent.onTyping, {
+                roomId: roomId,
+                typeTyping: 'stop',
+            } as IRequestOnTyping);
+        };
     }, [type]);
     return [type, setType] as [
         IRequestOnTyping['typeTyping'],
