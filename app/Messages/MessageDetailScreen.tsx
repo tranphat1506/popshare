03b0c6f135a6a4f1e46242b83dc5b68ff87fded4
Tran Phat <lechautranphat@gmail.com>
2024-09-23 08:19:26 +0700

    FIX: fix back button handler

--- app/Messages/MessageDetailScreen.tsx
+++ app/Messages/MessageDetailScreen.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MessageChatBoxLayout from '@/components/layout/MessageLayout';
 import { NavigationProp, RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '@/configs/routes.config';
 import MessageChatBox from '@/components/Messages/MessageChatBox';
+import { useAppDispatch } from '@/redux/hooks/hooks';
+import { updateChatStateData } from '@/redux/chatRoom/reducer';
+import { BackHandler } from 'react-native';
 function MessageDetailScreen() {
+    const dispatch = useAppDispatch();
     const navigation = useNavigation<NavigationProp<RootStackParamList, 'message-detail'>>();
     const { params: routeParams } = useRoute<RouteProp<RootStackParamList, 'message-detail'>>();
     const handleExit = () => {
         navigation.goBack();
+        return true;
     };
+    useEffect(() => {
+        // console.log('Set on chat room');
+        // Set state user is on chatting
+        dispatch(updateChatStateData({ field: 'isOnChatRoom', data: true }));
+        const backHandler = BackHandler.addEventListener('hardwareBackPress', handleExit);
+        return () => {
+            dispatch(updateChatStateData({ field: 'isOnChatRoom', data: false }));
+            backHandler.remove();
+        };
+    }, [dispatch]);
     if (!routeParams) return null;
     return (
         <>
